787f7d82a695cddd0d6578150a2f68c04ad76ee0
makurahumi <ethelandce.01kannna@gmail.com>
2020-07-05 22:43:19 +0900

    add_task_model_callback
    nameが空の場合、コールバックを追加。（コールバックの実験のため、コメントアウトで昨日しないようにしておく）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,17 @@
 class Task < ApplicationRecord
-  validates :name, presence: true #nameが空の場合、エラーを返す。
-  validates :name, length: { maximum: 30 } #nameが30文字以上の時、エラーを返す
 
-  validate :validate_name_not_including_comma
+  # before_validation :set_nameless_name #nameが空の場合、「名前なし」と自動入力する。
+
+  validates :name, presence: true #nameが空の場合、エラーを返す。
+  validates :name, length: { maximum: 30 } #nameが30文字以上の時、エラーを返す。
+
+  validate :validate_name_not_including_comma #カンマが入っていたら、エラーを返す。
 
   private
+
+  # def set_nameless_name
+  #   self.name = '名前なし' if name.blank?
+  # end
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
