616adb63118ea3ed0c0825f7e29b06d3b037b4d9
makurahumi <ethelandce.01kannna@gmail.com>
2020-07-05 22:27:25 +0900

    add_comma_validate
    コンマが入っていた場合、保存できないバリデーションを追加。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   validates :name, presence: true #nameが空の場合、エラーを返す。
   validates :name, length: { maximum: 30 } #nameが30文字以上の時、エラーを返す
+
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
